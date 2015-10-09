9dd5a8cf07fa4761085f769c1adb5dc757267e96
NJonsey <nyejones@hotmail.co.uk>
2015-10-09 15:57:47 +0100

    W-A-M grid and sudo

---
+++ app.js
@@ -0,0 +1,24 @@
+// Whack-A-Mole!!
+// Game board - grid/table.
+// Start Button.
+// Series of mole buttons
+// Hidden mole button - when user clicks start button, first mole button pops up.
+// Mole is on screen for x amount of time. 
+// When user clicks on mole, mole dissapears. 
+// click on mole triggers timer on next mole.
+// Whack counter, when mole gets clicked counter increases by one.
+// Next mole button reappears after x seconds in a random place for slightly less time (bonus feature).
+// This continues with the time each mole button is on the screen for decreasing (bonus feature), as 
+// does the time between each button appearing.
+// Game stops once mole disappears without being clicked. Alert pop up with 'play again?' option.
+
+// Additonal features:
+
+// Theme.
+// Mole images.
+// Whack sound.
+// whack animation.
+
+$(function(){
+
+}
