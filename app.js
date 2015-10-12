ab85760b5128f711f5a2bbdc6a55f87461508c58
NJonsey <nyejones@hotmail.co.uk>
2015-10-12 18:59:10 +0100

    moles appearing randomly and whacking

--- app.js
+++ app.js
@@ -22,37 +22,87 @@
 $(function(){
   console.log("hi");
 
+  var gamePlaying = false;
 
-//fade out buttons when play is clicked.
-$('#new_game').on("click", function(){
-  $('li').each(function(i){
-    $(this).fadeOut();
-    console.log("hello")
+
+  //fade out buttons when play is clicked.
+  $('#new_game').on("click", function(){
+    $('li').each(function(i){
+      $(this).fadeOut();
+      console.log("hello")
+    }) 
+  })   
+
+  var holes = $(".mole");
+  var delay = 1000;
+  // var random = Math.floor((Math.random()*holes.length)+1);      
+
+  // Make random mole visible   
+  $('#play').on("click", function(){
+    gamePlaying = true;
+    setTimeout(function() {
+      console.log("GAME ENDED");
+      gamePlaying = false;
+    }, 60000)
+    fadeInRandom();
   }) 
-})   
-    
-    var holes = $(".mole");
-    var random = Math.floor((Math.random()*holes.length)+1);      
 
- // Make random mole visible   
-$('#play').on("click", function(){
-$(holes[random]).fadeIn();
-  console.log("howdy")
-  }) 
-
-//when mole is clicked on it dissapears.
+  //when mole is clicked on it dissapears.
   $('li').on("click", function() {
-    $(this).fadeOut(), fadeRandom();
-  })
+    $(this).fadeOut(10); 
     
-//mole then reappears randomly.    
-  function fadeRandom() {
-    var random = Math.floor((Math.random()*holes.length)+1);
-    $(holes[random]).fadeIn();
-    console.log(random)
-    console.log(holes[random])
-    console.log("holla!")
-  
-}
+  })
 
-})
+  //mole then reappears randomly.    
+  var fadeInRandom = function() {
+    console.log("fadeInRandom", gamePlaying);
+    var random = Math.floor(Math.random()*(holes.length-1));
+    console.log(holes[random]);
+    $(holes[random]).fadeIn(delay, function(){
+      $(this).fadeOut(100);
+      delay = delay - 5;
+      console.log("see")
+      if (gamePlaying === true) {
+        return fadeInRandom();
+      }
+    });
+
+
+  };
+});
+
+// var gamePlaying = true;
+
+// function randomCall() {
+//   setTimeout(function() {
+//     console.log("HI THERE!");
+//     if(gamePlaying) {
+//       randomCall();
+//     }
+//   }, Math.floor(Math.random()*1000));
+// }
+
+// randomCall();
+
+// setTimeout(function() {
+//   gamePlaying = false;
+// }, 10000);
+
+// setInterval(function() {
+//   delay = Math.floor(Math.random()*1000);
+// }, delay);
+
+//   function fadeOutRandom() {
+//     var random = Math.floor((Math.random()*holes.length)+1);
+//     $(holes[random]).fadeOut(setTimeout(function(){2000});
+//     console.log(random)
+//   }); 
+// });
+    // set timer that will fade out the cell displayed, pick the value corresponding to the current inde
+//     // displayTimes[currentDisplayIndex] and set a timeout with this value, the 
+//     console.log(holes[random])
+//     console.log("holla!")
+    
+//   }
+
+// })
